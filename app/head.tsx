d0cf9c2e122ce9ff27c6842d90a462a5e7a523d9
Muhammad Usama <usamajhamat111@gmail.com>
2023-11-02 00:18:36 -0700

    Initial Commit

---
+++ app/head.tsx
@@ -0,0 +1,14 @@
+
+
+const Head = () => {
+  return (
+    <>
+        <title>NCAAM Scores and Schedule</title>
+        <meta content="width=device-wide, initial-scale-1" name="viewport"/>
+        <meta name="description" content="Like ESPN, but streamlined"/>
+        <link rel="icon" href="/favicon.ico"/>
+    </>
+  )
+}
+
+export default Head
--- next.config.js
+++ next.config.js
@@ -1,4 +1,29 @@
 /** @type {import('next').NextConfig} */
-const nextConfig = {}
-
-module.exports = nextConfig
+const nextConfig = {
+    images: {
+      // Example: https://a.espncdn.com/i/teamlogos/ncaa/500/66.png
+      // Default: https://a.espncdn.com/i/teamlogos/default-team-logo-500.png
+      remotePatterns: [
+        {
+          protocol: 'https',
+          hostname: 'a.espncdn.com',
+          port: '',
+          pathname: '/i/teamlogos/**',
+        },
+      ],
+      formats: ['image/avif', 'image/webp'],
+    },
+    experimental: {
+      appDir: true,
+      fetchCache: true,
+    },
+    // redirects: () => [
+    //   {
+    //     source: '/',
+    //     destination: '/66', // Default to ISU
+    //     permanent: false,
+    //   },
+    // ],
+  };
+  
+  module.exports = nextConfig;
